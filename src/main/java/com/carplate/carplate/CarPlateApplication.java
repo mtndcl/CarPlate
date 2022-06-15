385bcfcf0b8c0a4f05d50319cef3e0db353a0580
Metin DEMİR <metindemir1215.md@gmail.com>
2022-06-15 22:00:39 +0300

    Initial commit

---
+++ src/main/java/com/carplate/carplate/CarPlateApplication.java
@@ -0,0 +1,13 @@
+package com.carplate.carplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carplate/carplate/CarPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carplate.carplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
