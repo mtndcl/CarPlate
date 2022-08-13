d0d2a832fe5b3e01881e46359ec02e084c987f24
Metin DEMİR <metindemir1215.md@gmail.com>
2022-08-13 17:23:03 +0300

    Added default car plate to database

--- car-plate-backend/src/main/resources/import.sql
+++ car-plate-backend/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO person (id, username, password,created_date) VALUES (1, 'ahmet', 'MyPassord','2001-07-13');
 INSERT INTO person (id, username, password,created_date) VALUES (2, 'Metin', 'Thpassword','2001-07-13');
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (1, '42EGK79', '5',1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (2, '06EGK79', '5',1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (3, '07EGK79', 4.4,1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (4, '11EGK79', 3,1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (5, '34EGK79',2,1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (6, '42YG79', 3,1);
+INSERT INTO plate (id, plate_number, star,star_count) VALUES (7, '06EGR78', 3,1);
